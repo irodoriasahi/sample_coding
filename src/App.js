1671ba09c51cc26829590da256c25556febf34d2
irodoriasahi <irodori.asahi@gmail.com>
2022-02-16 20:34:41 +0900

    header作成中

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import { Fv } from "./components/Fv";
+import styled from "styled-components";
+
+import { Header } from "./components/Header";
 //コンポーネントをインポートするときは{}で囲む
 
 export const App = () => {
 
   return (
     <>
-    <Fv />
+
+    <Header />
+
 
     </>
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+import logo from "./img/logo.png";
+import mv from "./img/mv.jpg";
+//画像等をインポートするときは{}はいらない
+
+export const Fv = () => {
+
+  return (
+    <SContainer>
+      <MVimage src={mv} alt="logo" />
+      <SLogoContainer>
+      <Logo src={logo} alt="logoimagae" />
+      <p>COMING SOON</p>
+      </SLogoContainer>
+    </SContainer>
+  );
+};
+
+const SContainer = styled.div`
+  position: relative;
+  text-align: center;
+`;
+
+const MVimage = styled.img`
+
+  width: 100%;
+`;
+
+const Logo = styled.img`
+  width: 50%;
+`;
+
+const SLogoContainer = styled.div`
+  position: absolute;
+  text-align: center;
+  top:10vh;
+  left: 50%;
+`;
+
