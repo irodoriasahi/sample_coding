5a4db13023f9501ac4b43e618495e5b4070827d5
irodoriasahi <irodori.asahi@gmail.com>
2022-02-27 20:50:01 +0900

    Buttonにpropsで背景カラー情報を渡す

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ export const App = () => {
     <Header />
     <SMain>
       <Section />
-      <Button text="BgPink"/>
+      <Button color="#F1A0C3"/>
     </SMain>
     <Footer />
     </>
--- src/components/Button_css分けるver.jsx
+++ src/components/Button_css分けるver.jsx
@@ -2,11 +2,21 @@ import styled from "styled-components";
 
 export const Button = (props) => {
     return (
-      <SButton type="button" {...props}>BUTTON</SButton>
+      <SButton type="button" className={props.text}>BUTTON</SButton>
       );
 };
 
 const SButton = styled.button`
+  &.BgPink {
+    background-color: #F1A0C3;
+  }
+  &.BgBlue {
+    background-color: #91E6EC;
+  }
+  &.BgGreen {
+    background-color: #A8ECA2;
+  }
+
   width: 65%;
   max-width: 390px;
   margin-top: 20px;
@@ -14,7 +24,6 @@ const SButton = styled.button`
   border-radius: 6px;
   padding: 8px 0;
   color: #fff;
-  background-color: ${(props)=>props.color||"black"};
   font-size: 18px;
   font-weight: bold;
 `;
